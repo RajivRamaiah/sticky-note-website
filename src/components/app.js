17173ffe6d57a8975f0b57943b640766e5ce03ec
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-25 17:13:58 -0400

    completed firebase integration

--- src/components/app.js
+++ src/components/app.js
@@ -3,72 +3,99 @@ import CreateBar from './create_bar.js';
 import Note from './note.js';
 import Welcome from './welcome.js';
 import Immutable from 'immutable';
+import * as firebase from '../firebasedb';
 
 // example class based component (smart component)
 class App extends Component {
   constructor(props) {
     super(props);
 
-    // init component state here
     this.state = {
-      notes: Immutable.Map({
-        1: {
-          title: 'Hello',
-          text: 'I\'m a note, you can add more of me by typing a title and pressing the create button above!',
-          x: 0,
-          y: 0,
-          zIndex: 1,
-        },
-        2: {
-          title: 'Tips',
-          text: 'Move me around by dragging me from the arrows, or edit me by pressing the pencil button',
-          x: 300,
-          y: 150,
-          zIndex: 2,
-        },
-        3: {
-          title: 'Add Cool GIFs!',
-          text: '![](http://i.giphy.com/gyRWkLSQVqlPi.gif)',
-          x: 400,
-          y: 250,
-          zIndex: 3,
-        },
-      }),
-      nextID: 4,
-      zAxis: 4,
+      notes: Immutable.Map(),
     };
+    // init component state here
+    // this.state = {
+    //   notes: Immutable.Map({
+    //     1: {
+    //       title: 'Hello',
+    //       text: 'I\'m a note, you can add more of me by typing a title and pressing the create button above!',
+    //       x: 0,
+    //       y: 0,
+    //       zIndex: 1,
+    //     },
+    //     2: {
+    //       title: 'Tips',
+    //       text: 'Move me around by dragging me from the arrows, or edit me by pressing the pencil button',
+    //       x: 300,
+    //       y: 150,
+    //       zIndex: 2,
+    //     },
+    //     3: {
+    //       title: 'Add Cool GIFs!',
+    //       text: '![](http://i.giphy.com/gyRWkLSQVqlPi.gif)',
+    //       x: 400,
+    //       y: 250,
+    //       zIndex: 3,
+    //     },
+    //   }),
+    //   nextID: 4,
+    //   zAxis: 4,
+    // };
+    this.handleNotes = this.handleNotes.bind(this);
+  }
+
+  componentDidMount() {
+    firebase.onNotesChanged(this.handleNotes);
+  }
+
+  handleNotes(notes) {
+    this.setState({
+      notes: Immutable.Map(notes),
+    });
   }
 
   addNote(text) {
-    this.setState({
-      notes: this.state.notes.set(this.state.nextID, {
+    firebase.addNote(
+      {
         title: text,
         text: '',
         x: 0,
         y: 0,
-        zIndex: this.state.nextID,
-      }),
-      nextID: this.state.nextID + 1,
-      zAxis: this.state.zAxis + 1,
-    });
+        zIndex: 0,
+      }
+    );
+    // this.setState({
+    //   notes: this.state.notes.set(this.state.nextID, {
+    //     title: text,
+    //     text: '',
+    //     x: 0,
+    //     y: 0,
+    //     zIndex: this.state.nextID,
+    //   }),
+    //   nextID: this.state.nextID + 1,
+    //   zAxis: this.state.zAxis + 1,
+    // });
   }
 
   deleteNote(noteID) {
-    this.setState({
-      notes: this.state.notes.delete(noteID),
-    });
+    firebase.deleteNote(noteID);
+    // this.setState({
+    //   notes: this.state.notes.delete(noteID),
+    // });
   }
 
   updateText(text, id) {
-    this.setState({
-      notes: this.state.notes.update(id, (n) => { return Object.assign({}, n, { text }); }),
-    });
+    firebase.updateNote(id, { text });
+    // this.setState({
+    //   notes: this.state.notes.update(id, (n) => { return Object.assign({}, n, { text }); }),
+    // });
   }
 
   updatePosition(x, y, id) {
-    this.setState({
-      notes: this.state.notes.update(id, (n) => { return Object.assign({}, n, { x, y }); }),
-    });
+    firebase.updateNote(id, { x, y });
+    // this.setState({
+    //   notes: this.state.notes.update(id, (n) => { return Object.assign({}, n, { x, y }); }),
+    // });
   }
 
   displayNotes() {
--- src/firebasedb.js
+++ src/firebasedb.js
@@ -1,4 +1,31 @@
 import firebase from 'firebase';
 
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyA5hHFYKk444bwsXYACWha9fuHN6G-gnXk',
+  authDomain: 'sticky-note-central.firebaseapp.com',
+  databaseURL: 'https://sticky-note-central.firebaseio.com',
+  storageBucket: '',
+};
 
-export function fetchNotes(callback) { /* ... */ }
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export function deleteNote(id) {
+  database.ref('id').remove();
+}
+
+export function updateNote(id, data) {
+  database.ref('notes').child(id).update(data);
+}
+
+export function addNote(note) {
+  database.ref('notes').push(note);
+}
+
+export function onNotesChanged(handleNotes) {
+  database.ref('notes').on('value', (snapshot) => {
+    handleNotes(snapshot.val());
+  });
+}
